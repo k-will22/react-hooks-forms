2b07c8af86c3cbccab6ce5cd380af0c1170fc5aa
k-will22 <thestreetkw@hotmail.com>
2021-01-12 21:20:48 -0500

    commit tree bbef24187e65f4e2ebdf09858d650e616a58255b

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,48 @@
 import React, { useState } from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+function Form(props) {
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry",
+    admin: false,
+});
+
+function handleChange(event) {
+    const name = event.target.name;
+    let value = event.target.value;
+
+    if (event.target.type === "checkbox") {
+        value = event.target.checked;
+    }
+
+    setFormData({
+        ...formData,
+        [name]: value,
+    });
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData)
+}
+
+// return (
+//     <div>
+//       <Form
+//         firstName={firstName}
+//         lastName={lastName}
+//         handleFirstNameChange={handleFirstNameChange}
+//         handleLastNameChange={handleLastNameChange}
+//       />
+//       <DisplayData firstName={firstName} lastName={lastName} />
+//     </div>
+// )
 
   return (
-    <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="firstName" onChange={handleChange} value={formData.firstName} />
+      <input type="text" name="lastName" onChange={handleChange} value={formData.lastName} />
+      <input type="checkbox" name="admin" onChange={handleChange} checked={formData.admin} />
       <button type="submit">Submit</button>
     </form>
   );
---
+++ src/components/ParentComponent.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Form from "./Form";
+import DisplayData from "./DisplayData";
+
+function ParentComponent() {
+    const [formData, setFormData] = useState({
+        firstName: "John",
+        lastName: "Henry",
+        admin: false,
+    });
+
+    function handleChange(event) {
+        const name = event.target.name;
+        let value = event.target.value;
+
+        if (event.target.type === "checkbox") {
+            value = event.target.checked;
+        }
+
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(formData)
+    }
+
+    return (
+        <div>
+          <Form
+            firstName={firstName}
+            lastName={lastName}
+            handleFirstNameChange={handleFirstNameChange}
+            handleLastNameChange={handleLastNameChange}
+          />
+          <DisplayData firstName={firstName} lastName={lastName} />
+        </div>
+    )
+}
+
+export default ParentComponent
